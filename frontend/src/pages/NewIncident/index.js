fd041c1461160e0f3d69f731840fa99203cd4dcb
CleberG <cgmbnu@gmail.com>
2020-06-10 19:59:07 -0300

    adicioando funções para funcionamento de cadastro de ong, login e incidents

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {FiArrowLeft} from 'react-icons/fi'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            })
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Erro ao cadastrar caso, tente novamente.')
+        }
+
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -15,7 +48,7 @@ export default function NewIncident() {
                     <h1> Cadastro novo caso</h1>
 
                     <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
-                     
+
 
                     <Link className="back-link" to="/profile" >
                         <FiArrowLeft size={16} color="#E02041" />
@@ -23,12 +56,21 @@ export default function NewIncident() {
                 </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Título do caso" />
-                    <textarea type="text" placeholder="Descrição" />
-                    <input placeholder="valor em reais" />
+                <form onSubmit={handleNewIncident}>
+                    <input
+                        placeholder="Título do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea
+                        placeholder="Descrição" type="text"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)} />
+                    <input
+                        placeholder="valor em reais"
+                        value={value}
+                        onChange={e => setValue(e.target.value)} />
                     <form>
-                        <button>Cancelar</button>
                         <button className="button">Cadastrar</button>
                     </form>
 
